a1d22b92873241f76120859085fc81b9905995d9
MattChen1021 <mattchen1021gmail.com>
2020-09-25 22:51:28 +0000

    created header component

---
+++ client/components/product-list-item.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class ProductListItem extends React.Component {
+  render() {
+    return (
+      <>
+        <div className="card">
+        </div>
+      </>
+    );
+  }
+}
