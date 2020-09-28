82a9294d72eff635ac0dbbdd4b92dd2ad2511df4
MattChen1021 <mattchen1021gmail.com>
2020-09-28 22:18:13 +0000

    Completed view-details-front-end

---
+++ client/components/product-details.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+export default class ProductDetails extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      product: null
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  componentDidMount() {
+    fetch(`/api/products/${this.props.view.productId}`)
+      .then(res => res.json())
+      .then(product => this.setState({
+        product: product
+      }));
+  }
+
+  handleClick() {
+    this.props.onClick('catalog', {});
+  }
+
+  render() {
+    const product = this.state.product;
+    if (product) {
+      return (
+        <div className="container details shadow-sm mt-5">
+          <div className="row">
+            <div className="col-4 mt-3">
+              <div onClick={this.handleClick}
+                className="price-color cursor">
+                {'< Back to Catalog'}
+              </div>
+              <img className="img-size mt-2" src={product.image} alt={product.name} />
+            </div>
+            <div className="col-7 mt-3">
+              <h2>{product.name}</h2>
+              <div className="price-color">{'$' + (product.price / 100).toFixed(2)}</div>
+              <hr></hr>
+              <p>{product.shortDescription}</p>
+            </div>
+          </div>
+          <div className="row">
+            <hr></hr>
+            <p className="paragraph-spacing">{product.longDescription}</p>
+          </div>
+        </div >
+      );
+    } else {
+      return (
+        null
+      );
+    }
+  }
+}
--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
 const ProductListItem = props => {
+
   return (
-    <div className="card card-width h-100 shadow-sm" >
+    <div onClick={props.onClick} className="card card-width h-100 shadow-sm cursor" >
       <img src={props.images} className="card-img-top img-size text-center"></img>
       <div className="card-body">
         <h5 className="card-title">{props.name}</h5>
